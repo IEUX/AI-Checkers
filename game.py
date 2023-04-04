5cacf439fd7ee390426a308cb0b31869644d3cf1
MPLASZCZYNSKI <matheo.plaszczynski@ynov.com>
2023-04-04 12:26:06 +0200

    Take and move

--- game.py
+++ game.py
@@ -3,7 +3,9 @@ import numpy as np
 
 class Damier(tk.Canvas):
     checkerBoard = np.zeros((10,10))
-    playerTurn = -1
+    playerTurn = 1
+    state = 0
+    lastPostion = None
     def __init__(self,parent, height, width,color1,color2):
         # attributs
         self.parent = parent
@@ -25,6 +27,7 @@ class Damier(tk.Canvas):
                         Damier.checkerBoard[j][i] = 1
         self.pack()
 
+    # UTILS
     def createPosMap(self):
         self.height
         self.width
@@ -50,33 +53,108 @@ class Damier(tk.Canvas):
                     self.create_oval(i*self.height/10,j*self.width/10,(i+1)*self.height/10,(j+1)*self.width/10,fill="white")
 
     def getCase(self,event):
-        print("clic")
         x = event.x
         y = event.y
         positions = self.createPosMap()
         for i in range(10):
             for j in range(10):
-                if x > positions[i][j][0] and x < positions[i][j][1] and y > positions[i][j][2] and y < positions[i][j][3]:
+                if x > positions[i][j][0] and x <= positions[i][j][1] and y > positions[i][j][2] and y <= positions[i][j][3]:
                     return (i,j)
+    
+    # MOVEMENTS
 
     def choosePion(self,event):
         case = self.getCase(event) 
-        if self.checkerBoard[case[1]][case[0]] == self.playerTurn:
-            return case 
+        x = case[1]
+        y = case[0]
+        if self.checkerBoard[x][y] == self.playerTurn:
+            if x == 0:
+                if self.checkRight(x,y):
+                    return case
+            if x == 9:
+                if self.checkLeft(x,y):
+                    return case
+            if self.checkLeft(x,y) or self.checkRight(x,y):
+                return case
+        return None
+    
+    def checkLeft(self,x,y):
+        if self.checkerBoard[x + -self.playerTurn, y - 1] != self.playerTurn:
+            return True
+        return False
 
-    def pionMove(self,pionPostion):
-        self.checkerBoard[pionPostion[1] + -self.playerTurn][pionPostion[0]] = self.playerTurn
-            
-            
+    def checkRight(self,x,y):
+        if self.checkerBoard[x + -self.playerTurn, y + 1] != self.playerTurn:
+            return True
+        return False
+    
+    def moveLeft(self,x,y):
+        if self.checkerBoard[x,y] == 0 and self.lastPostion[0] - 1 == y and self.lastPostion[1] + -self.playerTurn == x:
+            return True
+        return False
+    
+    def moveRight(self,x,y):
+        if self.checkerBoard[x,y] == 0 and self.lastPostion[0] + 1 == y and self.lastPostion[1] + -self.playerTurn == x:
+            return True
+        return False
+    
+    def takeLeft(self,x,y):
+        if self.checkerBoard[x,y] == 0 and self.lastPostion[0] - 2 == y and self.lastPostion[1] + -2*self.playerTurn == x:
+            return True
+        return False
+    
+    def takeRight(self,x,y):
+        if self.checkerBoard[x,y] == 0 and self.lastPostion[0] + 2 == y and self.lastPostion[1] + -2*self.playerTurn == x:
+            return True
+        return False
+    
+
+    def chooseCase(self,event):
+        case = self.getCase(event)
+        x = case[1]
+        y = case[0]
+        if self.moveLeft(x,y): 
+            self.checkerBoard[x][y] = self.playerTurn
+            return True
+        if self.moveRight(x,y):
+            self.checkerBoard[x][y] = self.playerTurn
+            return True
+        if self.takeLeft(x,y):
+            self.checkerBoard[x][y] = self.playerTurn
+            print(self.lastPostion)
+            self.checkerBoard[self.lastPostion[1] + -self.playerTurn, self.lastPostion[0] - 1] = 0
+            return True
+        if self.takeRight(x,y):
+            self.checkerBoard[x][y] = self.playerTurn
+            self.checkerBoard[self.lastPostion[1] + -self.playerTurn, self.lastPostion[0] + 1] = 0
+            return True
+        return False
+        
+
+    def pionMove(self,event):
+        if self.chooseCase(event):
+            self.refreshMap()
+            self.state = 0
+            self.playerTurn = -self.playerTurn
+            self.checkerBoard[self.lastPostion[1]][self.lastPostion[0]] = 0
+            damier.refreshMap()
+        return
+
+    # GAMEPLAY     
+
     def turn(self,event):
-        pionPostion = self.choosePion(event)
-        if pionPostion == None:
-            return  
-        print(pionPostion)
-        self.checkerBoard[pionPostion[1]][pionPostion[0]] = 0
-        self.pionMove(pionPostion)
-        self.playerTurn = -self.playerTurn
-        damier.refreshMap()
+        # select pion
+        if self.state == 0:
+            pionPostion = self.choosePion(event)
+            if pionPostion == None:
+                return
+            self.lastPostion = pionPostion
+            self.state = 1
+            return
+        # select next case
+        if self.state == 1:
+            self.pionMove(event)
+            print(self.playerTurn)
 
 fenetre = tk.Tk()
 damier = Damier(fenetre,500,500,"tan1","tan4")
